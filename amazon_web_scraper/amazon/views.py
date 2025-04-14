087d2655dfb445ca0184acd04f3531c166f6ef63
nschlues <nschlues@uccs.edu>
2025-04-14 11:15:59 -0600

    Initial View/Template setup
    Did a bit more Django setup too

--- amazon_web_scraper/amazon/views.py
+++ amazon_web_scraper/amazon/views.py
@@ -1,3 +1,26 @@
+## Imports
 from django.shortcuts import render
 
-# Create your views here.
+## View functions
+# View for the initial welcome page. Basically
+# it just displays the view.
+def welcome(request):
+    return render(request, 'amazon_task1/welcome_template.html')
+
+
+
+
+
+# View for the page getting the user's search. 
+# Returns the search template
+def get_search(request):
+    return render(request, 'amazon_task1/search_page.html')
+
+
+# Need to do error checking and eventually 
+# filtering here. Go to the page displaying 
+# the results after a valid search or the
+# error page.
+def display_results(request):
+
+
