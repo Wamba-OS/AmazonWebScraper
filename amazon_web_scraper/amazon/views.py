7e7c8cf052363b155f73cb3a2e18c43d8170468f
nschlues <nschlues@uccs.edu>
2025-04-15 16:07:34 -0600

    Finished Task 1 and 2 to accommodate test cases

--- amazon_web_scraper/amazon/views.py
+++ amazon_web_scraper/amazon/views.py
@@ -1,9 +1,24 @@
 ## Imports
+import string
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from urllib.parse import quote_plus
 from .forms import SearchForm
 
+## Extra functions
+# Function for checking user input so we have test cases
+def check_alphanumeric(string):
+    for char in string:
+        if not (char.isalnum() or char.isspace()):
+            return False
+    return True
+
+# Function to check if there is at least one character
+def check_for_word(string):
+    for letter in string:
+        if letter.isalpha():
+            return True
+    return False
 
 
 ## View functions
@@ -39,21 +54,36 @@ def display_results(request):
         if form.is_valid():
             search = form.cleaned_data['search']
 
+            # Error Checking
+            if not check_for_word(search):
+                form.add_error('search', 'Search must contain at least one letter')
+                return render(request, 'amazon_task1/search_page.html', {'form': form})
+            
+            warning_enabled = not check_alphanumeric(search)
+
+
             # Formulate the search
             formatted_search = quote_plus(search)
-
-
-            # Error Checking
 
 
             # Encode the URL
             url = f"https://www.amazon.com/s?k={formatted_search}"
 
-            # Display results
-            return render(request, 'amazon_task1/results_page.html', {
+
+            # Create context dictionary
+            context = {
                 'search': search,
-                'URL': url
-            })
+                'URL': url,
+            }
+            
+            # Only add warning message if special characters are present
+            if warning_enabled:
+                context['warning_enabled'] = True
+                context['special_chars_warning'] = "Your search contains special characters. Amazon may handle these differently than expected."
+            
+            # Display results with warning if needed
+            return render(request, 'amazon_task1/results_page.html', context)
+
         
         # Else if form is invalid
         return render(request, 'amazon_task1/search_page.html', {'form': form})
